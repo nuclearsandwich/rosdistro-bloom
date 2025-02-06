ffac976d96d62a66799658f13dcb55918d13c2af
Steven! Ragnarök <steven@nuclearsandwich.com>
2025-02-05 23:02:52 -0800

    Add a test for migrating patches when updating target platform.

--- test/test_bloom_release.py
+++ test/test_bloom_release.py
@@ -88,3 +88,21 @@ def test_bloom_release_new_distro_patches(tmp_path, rosdistro):
             with inbranch("rpm/ros-jazzy-testpkg-1.0.0-3_9"):
                 assert (tmp_path / bloom_repo.release_repo / "releasepatch").is_file()
                 assert (tmp_path / bloom_repo.release_repo / "rpmpatch").is_file()
+
+
+def test_bloom_release_new_platform_patches(tmp_path, rosdistro, monkeypatch):
+    monkeypatch.setenv("ROSDISTRO_INDEX_URL", "https://raw.githubusercontent.com/rosdistro-bloom-testing/rosdistro/humble-to-noble/index-v4.yaml")
+    source_dist = rosdistro("humble", {"testpkg": {"type": "git", "version": "1.0.0-2", "url": "https://github.com/rosdistro-bloom-testing/patchpkg-release.git"}})
+    dest_dist = rosdistro("humble", {"testpkg": {"type": "git", "version": "1.0.0-2", "url": "https://github.com/rosdistro-bloom-testing/patchpkg-release.git"}})
+    with chdir(tmp_path):
+        with GitReleaseRepository("testpkg", source_dist, dest_dist).clone() as bloom_repo:
+            bloom_repo.bloom_release()
+            assert dest_dist.repositories["testpkg"].release_repository.version == "1.0.0-3"
+            with inbranch("release/humble/testpkg/1.0.0-3"):
+                assert (tmp_path / bloom_repo.release_repo / "releasepatch").is_file()
+            with inbranch("debian/ros-humble-testpkg_1.0.0-3_noble"):
+                assert (tmp_path / bloom_repo.release_repo / "releasepatch").is_file()
+                assert (tmp_path / bloom_repo.release_repo / "debianpatch").is_file()
+            with inbranch("rpm/ros-humble-testpkg-1.0.0-3_8"):
+                assert (tmp_path / bloom_repo.release_repo / "releasepatch").is_file()
+                assert (tmp_path / bloom_repo.release_repo / "rpmpatch").is_file()

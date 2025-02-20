238af4d6898d576a48f0d1e0526adf0470d518e1
Steven! Ragnarök <steven@nuclearsandwich.com>
2025-02-20 08:49:35 -0800

    Reformat file using ruff.
    Format-only-change: true
    Formatted-by: ruff 0.9.2

--- rosdistro_bloom/github.py
+++ rosdistro_bloom/github.py
@@ -15,24 +15,26 @@ class ReleaseOrg:
         if not repository_url.startswith(f"https://github.com/{self.name}/"):
             return False
         repo_name = repository_url.split("/")[-1]
-        if repo_name.endswith('.git'):
+        if repo_name.endswith(".git"):
             repo_name = repo_name[:-4]
         if repo_name in self.repository_names:
             return True
         else:
             return False
 
+
 def can_release_repo(release_repository_spec):
     if release_repository_spec.type != "git":
         return False
 
-def github_client(token = None):
+
+def github_client(token=None):
     if token is None:
         token = os.environ["GITHUB_TOKEN"]
     return github.Github(token)
 
 
-def check_release_(dist: DistributionFile, org_name: str, repo_names = None):
+def check_release_(dist: DistributionFile, org_name: str, repo_names=None):
     errors = []
     gclient = github.Github(os.environ["GITHUB_TOKEN"])
     release_org = gclient.get_organization(org_name)
@@ -56,11 +58,10 @@ def check_release_(dist: DistributionFile, org_name: str, repo_names = None):
             errors.append(ValueError(f"{repo_name} has a release repository outside the release org"))
 
         gh_repo_name = release_repository.url.split("/")[-1]
-        if gh_repo_name.endswith('.git'):
+        if gh_repo_name.endswith(".git"):
             gh_repo_name = gh_repo_name[:-4]
         if gh_repo_name not in org_release_repos:
             errors.append(ValueError(f"{repo_name} does not exist in release org"))
             continue
 
     return errors
-
